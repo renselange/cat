16da57c0fae61bbc50ce84703b2e00c57b18e3ca
Rense Lange <renselange@gmail.com>
2022-06-07 14:52:44 +0800

    Update runcat.py

--- runcat.py
+++ runcat.py
@@ -2,12 +2,17 @@
 
 import streamlit as st
 
+q = ['question_%d'%d for d in range(3)]
+
 st.title('Counter Example')
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
 increment = st.button('Increment')
+decremnet = st.button('Decrement')
+
 if increment:
-    st.session_state.count += 1
+    st.session_state.count = (st.session_state.count +1)%len(q)
+
 
 st.write('Count = ', st.session_state.count)
